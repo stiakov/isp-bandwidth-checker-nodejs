be01c0e89773e611d856ee1cf203291278246ba2
Santiago Torres G <thdealer@gmail.com>
2019-08-28 16:37:45 -0500

    updating module exports and index imports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { db_name, lang, timezone, encoding, speedtest_json } from 'settings.js'
+const set = require('./settings.js');
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync(db_name);
+const adapter = new FileSync(set.db_name);
 const db = low(adapter);
 db.defaults({ registers: [], count: 0 }).write();
 
@@ -11,10 +11,10 @@ const execSync = require('child_process').execSync;
 const check_bandwidth = (random_timer = (Math.round(Math.random() * 11) + 1) * 60000) => {
   
   setTimeout(() => {
-    const date_time = new Date().toLocaleString(lang, timezone);
-    const stdout = execSync(speedtest_json, encoding);
+    const date_time = new Date().toLocaleString(set.lang, set.timezone);
+    const stdout = execSync(set.speedtest_json, set.encoding);
     const result = JSON.parse(stdout);
-    const finish_time = new Date().toLocaleString(lang, timezone);
+    const finish_time = new Date().toLocaleString(set.lang, set.timezone);
     
     db.get('registers')
     .push({
@@ -42,12 +42,10 @@ const check_bandwidth = (random_timer = (Math.round(Math.random() * 11) + 1) * 6
   }, random_timer);
 };
 
-
 try {
   check_bandwidth(0);
   setInterval(check_bandwidth, 24 * 60000);
   
 } catch (error) {
-  execSync('echo $error >> error.log', { encoding: 'utf-8' });
+  execSync('echo $error >> error.log', set.encoding);
 }
-
