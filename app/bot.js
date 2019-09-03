77114efb515fa2fcf070d4250bf5708bd66d6b9e
Santiago Torres G <thdealer@gmail.com>
2019-09-03 15:56:42 -0500

    minor fi

--- app/bot.js
+++ app/bot.js
@@ -37,10 +37,7 @@ my_bot.on('message', (msg) => {
     console.log('sending IP');
     send_data(msg.chat.id, `Server IP: ${ip[0].client.ip}`);
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 4f19ecbe4450ba713a006bd58e4533066be38461
   if (msg.text.toString().toLowerCase().includes('telegram')) {
     const chatId = msg.chat.id;
     const message = `Your Telegram ID is: ${chatId}`;
